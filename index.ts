f2e93a3cd39c5c4b4724720a44f8b7ce90da7e31
a0m0rajab <rajab-2@windowslive.com>
2025-07-22 23:49:36 +0300

    chore: convert to typescript

--- index.ts
+++ index.ts
@@ -1,33 +1,36 @@
-// server.js
-const whatsappWebJs = require("whatsapp-web.js");
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+import { Client, NoAuth } from "whatsapp-web.js";
+import express, { Application } from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
 
-const app = express();
+const app: Application = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static("public")); // Serve static frontend
 
-io.on("connection", (socket) => {
+io.on("connection", (socket: Socket) => {
     console.log("Client connected");
-    const client = new whatsappWebJs.Client({
-        authStrategy: new whatsappWebJs.NoAuth(),
+
+    const client = new Client({
+        authStrategy: new NoAuth(),
         puppeteer: { headless: true },
     });
-    client.on("qr", (qr) => {
+
+    client.on("qr", (qr: string) => {
         console.log("QR Code regenerated");
         socket.emit("qrCode", qr);
     });
+
     client.on("ready", () => {
         console.log("WhatsApp client is ready!");
         socket.emit("clientReady");
     });
+
     client.initialize();
 });
 
-const PORT = 3001;
+const PORT: number = 3001;
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
